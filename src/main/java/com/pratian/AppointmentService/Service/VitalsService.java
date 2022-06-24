1fb21131f90a9e2159e6a0fc6ec1b8a81e97b7a3
siva.sandeep <siva.sandeep@skillassure.com>
2022-06-24 15:58:47 +0000

    Update VitalsService.java

--- src/main/java/com/pratian/AppointmentService/Service/VitalsService.java
+++ src/main/java/com/pratian/AppointmentService/Service/VitalsService.java
@@ -6,15 +6,15 @@ import com.pratian.AppointmentService.Entities.Vitals;
 import com.pratian.AppointmentService.Exceptions.VitalsNotFoundException;
 
 public interface VitalsService {
-	
-public List<Vitals>getVitals();
-	
+
+	public List<Vitals>getVitals() throws VitalsNotFoundException;
+
 	public Vitals getVitalsById(long vitalsId) throws VitalsNotFoundException;
 	public Vitals saveVital(Vitals vital) throws VitalsNotFoundException;
 	//public Vitals updateVital(Vitals vital);
 	public Vitals updateVital( long vitalsID ,Vitals vital) throws VitalsNotFoundException;
-	
-	
-	
+
+
+
 
 }
