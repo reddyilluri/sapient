417c9c14917ffac7982dd7f5e11f3630fb448e19
jai.surya.r.k <jai.surya.r.k@skillassure.com>
2022-06-25 08:04:12 +0000

    Update CorsConfig.java

--- src/main/java/com/pratian/AppointmentService/config/CorsConfig.java
+++ src/main/java/com/pratian/AppointmentService/config/CorsConfig.java
@@ -1,22 +1,24 @@
 package com.pratian.AppointmentService.config;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
-@Profile("CorsConfigure")
-
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 	
+	@Bean
+	public WebMvcConfigurer crosConfigure() {
+		return new WebMvcConfigurer() {
+			 @Override
+			    public void addCorsMappings(CorsRegistry registry) {
+			        registry.addMapping("/**")
+			                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+			                .allowedHeaders("*").allowedOriginPatterns("*");
+			    }
+		};
+	}
 	
-	 @Override
-	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**")
-	                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-	                .allowedHeaders("*").allowedOrigins("*");
-	    }
 	}
 
 
