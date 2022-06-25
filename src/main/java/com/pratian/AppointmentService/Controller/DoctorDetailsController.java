b33c8895fe312ba691f65dee4a8b859a1582c018
siva.sandeep <siva.sandeep@skillassure.com>
2022-06-25 05:44:56 +0000

    Update DoctorDetailsController.java

--- src/main/java/com/pratian/AppointmentService/Controller/DoctorDetailsController.java
+++ src/main/java/com/pratian/AppointmentService/Controller/DoctorDetailsController.java
@@ -11,8 +11,8 @@ import com.pratian.AppointmentService.Entities.DoctorDetails;
 import com.pratian.AppointmentService.Service.DoctorDetailsService;
 
 
-@RestController
 @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true", allowedHeaders = "*", exposedHeaders = "If_Match")
+@RestController
 @RequestMapping(value="/appointment")
 
 //@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
