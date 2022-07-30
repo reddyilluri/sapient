ccbb7727f6ed3b347bfd0f3ea2226cae2ed92f50
Administrator <admin@example.com>
2022-07-30 12:04:40 +0000

    Update AppointmentServiceApplication.java

--- src/main/java/com/pratian/AppointmentService/AppointmentServiceApplication.java
+++ src/main/java/com/pratian/AppointmentService/AppointmentServiceApplication.java
@@ -18,7 +18,7 @@ import io.swagger.v3.oas.models.servers.Server;
 @SpringBootApplication
 //@EnableEurekaClient
 public class AppointmentServiceApplication {
-	@Value(value = "${swagger.url.productionURL}")
+	@Value(value = "${productionURL}")
     public String productionURL;
    // @Value(value = "${swagger.url.localURL}")
     //public String localURL;
