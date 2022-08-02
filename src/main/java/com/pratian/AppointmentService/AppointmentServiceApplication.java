04a255eba0b7e02a15a5c5a8d6492809ada3c94e
Administrator <admin@example.com>
2022-08-02 09:39:09 +0000

    Update AppointmentServiceApplication.java

--- src/main/java/com/pratian/AppointmentService/AppointmentServiceApplication.java
+++ src/main/java/com/pratian/AppointmentService/AppointmentServiceApplication.java
@@ -11,33 +11,29 @@ import org.springframework.context.annotation.Bean;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 
-
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-
 @SpringBootApplication
-//@EnableEurekaClient
-public class AppointmentServiceApplication {
+public class PatientServiceApplication{
 	@Value(value = "${swagger.url.productionURL}")
     public String productionURL;
-   // @Value(value = "${swagger.url.localURL}")
+    //@Value(value = "${swagger.url.localURL}")
     //public String localURL;
+
 	public static void main(String[] args) {
-		SpringApplication.run(AppointmentServiceApplication.class, args);	
+		SpringApplication.run(AppointmentServiceApplication.class, args);
 	}
+	
 	@Bean
     public OpenAPI customOpenAPI() {
-        Server productionserver = new Server();
-       // Server localserver = new Server();
+       Server productionserver = new Server();
+        //Server localserver = new Server();
         List<Server> servers = new ArrayList<>();
        productionserver.setUrl(productionURL);
-       // localserver.setUrl(localURL);
-        servers.add(productionserver);
-        //servers.add(localserver);
+        //localserver.setUrl(localURL);
+       servers.add(productionserver);
+       // servers.add(localserver);
         OpenAPI openAPI = new OpenAPI();
         openAPI.setServers(servers);
         return openAPI;
     }
 	
-
 }
